0d5c8a7038e180e0b9e1d8f38ad3f6a009fdf1e4
Jacks Who <191100479@qq.com>
2015-12-11 06:18:03 +0800

    [add]:init lab

---
+++ app/listview-react/require-config.js
@@ -0,0 +1,10 @@
+require.config({
+	baseUrl: "js",
+	dir: "../dest",
+	paths: {
+		'react': '../lib/react',
+		'jquery': '../lib/jquery-1.7.2.min',
+		'react-canvas': '../lib/react-canvas',
+		'react-router': '../lib/ReactRouter'
+	}
+})
