a970e388d222d86ce90c26c2145247cba68a761e
Jack <191100479@qq.com>
2017-03-25 23:49:37 +0800

    fix: my love

--- app/love-tree/js/index.js
+++ app/love-tree/js/index.js
@@ -126,7 +126,7 @@ var index = (function() {
       var girlName = args["girl"];
       $("#girl-name").text(girlName);
     }
-    var timeArray = new Array(2015, 06, 08);
+    var timeArray = new Array(2015, 10, 24);
     var timeIsRight = true;
     if (args["time"] !== undefined) {
       var reg = /^([1-2]\d{3})[\/|\-](0?[1-9]|10|11|12)[\/|\-]([1-2]?[0-9]|0[1-9]|30|31)$/;
