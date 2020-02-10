5b9fa34d105e14a68003eddd6530cf616bfc519d
Jack <191100479@qq.com>
2020-02-11 03:06:01 +0800

    docs(all): 格式化代码，app添加strml项目路径

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Lab
-> Lab for awesome effect
+> Lab for the collection of web page awesome effect.
 
-## Warn
-> There is no any compressed files
+## Code Clear
+> There is no any compressed files.
 
-## Thanks to this Crazy Lab
+## To Learn
+> These small experiments contain a lot of front-end development basic knowledge.
+> I hope it can be helpful for beginners and also give some inspiration to experts.
+
+## Thanks
+
+# 实验室
+> 这里收集了一些效果酷炫的网页。
+
+## 代码干净
+> 没有进行压缩
+
+## 去学习
+> 这些小实验包含了很多前端开发的基础知识。
+> 希望对初学者有所帮助，也能给高手一些启发。
+
+## 感谢
--- index/utility/component/Record_PV_by_LocalStorage.js
+++ index/utility/component/Record_PV_by_LocalStorage.js
@@ -2,56 +2,56 @@
  * @author:Jacks Who
  * time:2015-12-12
  **/
-var record_PV_by_LocalStorage = (function() {
-  var entry = function(pageName) {
+var record_PV_by_LocalStorage = (function () {
+  var entry        = function (pageName) {
     var objectPageInit = {
       pageView: 1,
       pageName: pageName
-    };
-    var objectAllPage = {};
+    }
+    var objectAllPage  = {}
     //获取对应页面的json对象
-    var objAllString = localStorage.getItem("objectAllPage");
+    var objAllString   = localStorage.getItem('objectAllPage')
     if (objAllString) {
       //把字符串转换成JSON对象
-      objectAllPage = JSON.parse(objAllString);
-      if (objectAllPage[pageName]) {
-        objectAllPage[pageName].pageView = Number(objectAllPage[pageName].pageView) + 1;
+      objectAllPage = JSON.parse(objAllString)
+      if (objectAllPage[ pageName ]) {
+        objectAllPage[ pageName ].pageView = Number(objectAllPage[ pageName ].pageView) + 1
       } else {
-        objectAllPage[pageName] = objectPageInit;
+        objectAllPage[ pageName ] = objectPageInit
       }
     } else {
-      objectAllPage[pageName] = objectPageInit;
+      objectAllPage[ pageName ] = objectPageInit
     }
     //将JSON对象转化成字符串
-    objAllString = JSON.stringify(objectAllPage);
-    localStorage.setItem("objectAllPage", objAllString);
-    return objectAllPage[pageName].pageView;
-  };
-  var HtmlPageView = function(pageName) {
-    var pageView = getPageView(pageName);
-    if (isNaN(pageView)) return pageView;
-    var div = document.createElement("div");
-    div.innerHTML  = "<span>你已经用该浏览器访问此页面</span>" + pageView +"次";
-    return div;
-  };
-  var getPageView = function(pageName) {
-    var objAllString = localStorage.getItem("objectAllPage");
+    objAllString = JSON.stringify(objectAllPage)
+    localStorage.setItem('objectAllPage', objAllString)
+    return objectAllPage[ pageName ].pageView
+  }
+  var HtmlPageView = function (pageName) {
+    var pageView = getPageView(pageName)
+    if (isNaN(pageView)) return pageView
+    var div       = document.createElement('div')
+    div.innerHTML = '<span>你已经用该浏览器访问此页面</span>' + pageView + '次'
+    return div
+  }
+  var getPageView  = function (pageName) {
+    var objAllString = localStorage.getItem('objectAllPage')
     if (objAllString) {
       //把字符串转换成JSON对象
-      objectAllPage = JSON.parse(objAllString);
-      if (objectAllPage[pageName]) {
-        return objectAllPage[pageName].pageView;
+      objectAllPage = JSON.parse(objAllString)
+      if (objectAllPage[ pageName ]) {
+        return objectAllPage[ pageName ].pageView
       } else {
-        return "the objectAllPage does not contain the pageName";
+        return 'the objectAllPage does not contain the pageName'
       }
     } else {
       //可能未记录为false
-      return "the objectAllPage is undefined";
+      return 'the objectAllPage is undefined'
     }
-  };
+  }
   return {
-    entry: entry,
+    entry       : entry,
     HtmlPageView: HtmlPageView,
-    getPageView: getPageView
-  };
-})();
+    getPageView : getPageView
+  }
+})()
